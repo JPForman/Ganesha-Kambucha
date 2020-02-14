78d122915000fabc7d5bf7a54f514a1c0d657c6d
Joshua Forman <cyborgforman@gmail.com>
2020-02-14 09:50:15 -0800

    create initial components for home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {HashRouter} from 'react-router-dom';
 
 
-RReactDOM.render(<HashRouter>
+ReactDOM.render(<HashRouter>
   <App />
   </HashRouter>,
    document.getElementById('root'));

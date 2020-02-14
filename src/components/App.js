30c49ebf8915be56f416c8e0690db196f31ed11d
Joshua Forman <cyborgforman@gmail.com>
2020-02-14 09:19:51 -0800

    update app with initial routing

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+}
+
+export default App;

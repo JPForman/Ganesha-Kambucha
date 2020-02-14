593cc83a8aeaf8d4a55743519679333d6db5d9ba
Joshua Forman <cyborgforman@gmail.com>
2020-02-14 14:56:49 -0800

    add table and some contents to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 function Home(){
   return(
     <div>
-      <h2>BREWS</h2>
+      <div>
+        <h2>BREWS</h2>
+      </div>
+
+
+      <table width="100%">
+        <tr>
+          <th>Name</th>
+          <th>Flavor</th>
+          <th>Price</th>
+          <th>Amount Left In Keg</th>
+          <th>Sell a Pint</th>
+          <th>Less Than a Pint Left!</th>
+        </tr>
+        <tr>
+
+        </tr>
+      </table>
     </div>
-  )
+  );
 }
 
 export default Home;

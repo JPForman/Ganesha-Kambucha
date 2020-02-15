50d3e916f9b8b9b2ab75420d5ab83d551a67eaa3
Joshua Forman <cyborgforman@gmail.com>
2020-02-14 21:47:30 -0800

    add styles and image to brew page

--- src/components/Brews.jsx
+++ src/components/Brews.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from "prop-types";
 import KombuchaList from './KombuchaList';
+import GaneshaBillboard from './images/GaneshaBillboard.jpg'
 
 
 function Brews(props){
+  const ganeshaBillboard={
+    height: '400px',
+    width: 'auto',
+    backgroundColor: '#c75d48',
+  }
 
   const kombuchaRow={
     display: 'grid',
@@ -15,6 +21,9 @@ function Brews(props){
     <div>
       <div>
         <h2>BREWS</h2>
+        <div >
+          <img style={ganeshaBillboard} src={GaneshaBillboard}></img>
+        </div>
         <Link to='./NewKombucha'>Add a Brew</Link>
       </div>
 

9c48386af24b68326f13f3f9eb9471ec59444d6a
Joshua Forman <cyborgforman@gmail.com>
2020-02-14 22:32:38 -0800

    add styles

--- src/components/Brews.jsx
+++ src/components/Brews.jsx
@@ -6,10 +6,27 @@ import GaneshaBillboard from './images/GaneshaBillboard.jpg'
 
 
 function Brews(props){
+
+  const billboard={
+    backgroundColor: '#c75d48',
+    marginTop: '90px',
+    height: 'auto',
+
+  }
+
+  const sectionTitle={
+    top: '50%',
+    left: '78%',
+    position: 'absolute',
+    fontSize: '60px',
+  }
+
   const ganeshaBillboard={
+    zIndex: '-1',
     height: '400px',
     width: 'auto',
     backgroundColor: '#c75d48',
+    border: '5px solid black',
   }
 
   const kombuchaRow={
@@ -20,11 +37,11 @@ function Brews(props){
   return(
     <div>
       <div>
-        <h2>BREWS</h2>
-        <div >
-          <img style={ganeshaBillboard} src={GaneshaBillboard}></img>
-        </div>
-        <Link to='./NewKombucha'>Add a Brew</Link>
+        <h2 style={sectionTitle}>BREWS</h2>
+          <div style={billboard}>
+            <img style={ganeshaBillboard} src={GaneshaBillboard}></img>
+          </div>
+          <Link to='./NewKombucha'>Add a Brew</Link>
       </div>
 
       <KombuchaList />

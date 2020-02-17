86237923e0887fc3ad8f4f9b74b6ef962d4d478a
Joshua Forman <cyborgforman@gmail.com>
2020-02-16 19:45:44 -0800

    add error 404 handling

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Brews from './Brews';
 import Header from './Header';
 import NewKombucha from './NewKombucha';
 import UpdateKombucha from './UpdateKombucha';
+import Error404 from './Error404';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route exact path="/" component={Brews} />
         <Route exact path="/NewKombucha" component={NewKombucha} />
         <Route exact path="/UpdateKombucha" component={UpdateKombucha} />
+        <Route component={Error404} />
 
       </Switch>
     </div>

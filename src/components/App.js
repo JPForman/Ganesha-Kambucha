9cf0783c0f41305241aa96c3773015f1ca143934
Joshua Forman <cyborgforman@gmail.com>
2020-02-21 10:28:57 -0800

    update app to a class with state that functionally handles adding new kombucha to master list

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,37 @@ import UpdateKombucha from './UpdateKombucha';
 import Error404 from './Error404';
 
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKombuchaList: []
+    };
+    this.handleAddingNewKombuchaToList = this.handleAddingNewKombuchaToList.bind(this);
+  }
+
+  handleAddingNewKombuchaToList(newBrew) {
+    var newMasterKombuchaList = this.state.masterKombuchaList.slice();
+    newMasterKombuchaList.push(newBrew);
+    this.setState({masterKombuchaList: newMasterKombuchaList});
+    console.log(this.state);
+  }
+
+  render(){
   return (
     <div className="App">
       <Header />
       <Switch>
         <Route exact path="/" component={Brews} />
-        <Route exact path="/NewKombucha" component={NewKombucha} />
+        <Route exact path="/NewKombucha" render={()=><NewKombucha onNewKombuchaCreation={this.handleAddingNewKombuchaToList} />} />
         <Route exact path="/UpdateKombucha" component={UpdateKombucha} />
         <Route component={Error404} />
 
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
